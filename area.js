b6d72c4ade600fc8825a996d328f448f50edce82
abana <neroabana@gmail.com>
2021-03-29 11:51:41 +0100

    refactored getArea

--- area.js
+++ area.js
@@ -1,4 +1,5 @@
 const getArea = () => {
+  
   let lastValidPoint = 698; //Robot will meet first barrier at 699 on every axis in every plane
   let validStep, gridValidSteps, finalValidSteps, totalValidSteps = 0;//initialize variables  
   
